36de72dadd66428a95445ad2bf3d455d24a7e102
Laura Davies <drljdavies@gmail.com>
2021-05-04 14:43:49 -1000

    Completed

--- data_structures_and_algo/divide_and_conquer.go
+++ data_structures_and_algo/divide_and_conquer.go
@@ -42,21 +42,25 @@ Algorithm:
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-func maxSubArray(nums []int) []int {
-	subs := []int{}
-	if len(nums) == 0 {
-		return subs
-	}
-
+func maxSubArray(nums []int) int {
 	sum := sumOfSubarray(nums)
 
-	subs = append(subs, sum)
-	subs = append(subs, maxSubArray(nums[1:])...)
-	subs = append(subs, maxSubArray(nums[:(len(nums)-1)])...)
+	if len(nums) == 1 {
+		return nums[0]
+	}
 
-	return subs
+	next := maxSubArray(nums[1:])
+	otherNext := maxSubArray(nums[:len(nums)-1])
+
+	val := []int{sum, next, otherNext}
+	sort.Ints(val)
+
+	return val[len(val)-1]
 }
 
 func sumOfSubarray(nums []int) (sum int) {
@@ -68,4 +72,11 @@ func sumOfSubarray(nums []int) (sum int) {
 
 func main() {
 	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	fmt.Println(maxSubArray([]int{5, 4, -1, 7, 8}))
+	fmt.Println(maxSubArray([]int{1}))
+	fmt.Println(maxSubArray([]int{-1}))
+	fmt.Println(maxSubArray([]int{-84, -87, -78, -16, -94, -36, -87, -93, -50, -22, -63, -28, -91, -60, -64, -27, -41, -27, -73, -37, -12, -69, -68, -30}))
+	//fmt.Println(maxSubArray([]int{-84, -87, -78, -16, -94, -36, -87, -93, -50, -22, -63, -28, -91, -60, -64, -27, -41, -27, -73, -37, -12, -69, -68, -30, -83, -31, -63, -24, -68, -36, -30, -3, -23, -59, -70, -68, -94, -57, -12, -43, -30, -74, -22, -20, -85, -38, -99, -25, -16, -71, -14, -27, -92, -81, -57, -74, -63, -71, -97, -82, -6, -26, -85, -28, -37, -6, -47, -30, -14, -58, -25, -96, -83, -46, -15, -68, -35, -65, -44, -51, -88, -9, -77, -79, -89, -85, -4, -52, -55, -100, -33, -61, -77, -69, -40, -13, -27, -87, -95, -40}))
 }
+
+// last test case times out
