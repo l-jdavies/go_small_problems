14721bd36e8f5d75c099ffb25210d60ba1f708c3
Laura Davies <drljdavies@gmail.com>
2021-05-05 10:30:34 -1000

    track tail node, delete from head or tail

--- data_structures_and_algo/double_linked_lists.go
+++ data_structures_and_algo/double_linked_lists.go
@@ -1,12 +1,17 @@
 /*
 With doubly linked lists, each node has a reference to the next and the previous node. This data structure is useful for efficiently adding and removing elements to the start and end of the linked list, whereas singly linked lists only have a time complexity of O(1) for adding/removing elements from the start of the list.
 */
+
+// track tail, delete from start and end
+
 package main
 
 import "fmt"
 
 type doubleLinkedList struct {
-	head *node
+	head   *node
+	tail   *node
+	length int
 }
 
 type node struct {
@@ -18,8 +23,9 @@ type node struct {
 // if linked list is empty, make node the head of the list
 // add node to start of the linkedList
 func (l *doubleLinkedList) addToStart(n *node) {
-	if l.head == nil {
+	if l.length == 0 {
 		l.head = n
+		l.tail = n
 	} else {
 		second := l.head
 		l.head = n
@@ -27,28 +33,31 @@ func (l *doubleLinkedList) addToStart(n *node) {
 		second.prev = l.head
 		l.head.next = second
 	}
+
+	l.length++
 }
 
 // finds the last element in the linked list. Update the `next` property of the last element to reference the inserted node
 func (l *doubleLinkedList) addToEnd(n *node) {
-	if l.head == nil {
-		fmt.Println("List is empty. Add node to start")
+	if l.length == 0 {
+		l.head = n
+		l.tail = n
+	} else {
+		secondLast := l.tail
+		l.tail = n
+		secondLast.next = l.tail
+		l.tail.prev = secondLast
 	}
 
-	last := l.findLastElement()
-	last.next = n
-	n.prev = last
+	l.length++
 }
 
-// traverse list until find node with a `next` property of nil and return
-func (l *doubleLinkedList) findLastElement() (n *node) {
-	n = l.head
+func (l *doubleLinkedList) printHead() {
+	fmt.Println(l.head)
+}
 
-	for n.next != nil {
-		n = n.next
-	}
-
-	return
+func (l *doubleLinkedList) printTail() {
+	fmt.Println(l.tail)
 }
 
 // return true if value is present in list, else return false
@@ -64,13 +73,52 @@ func (l *doubleLinkedList) findVal(val int) bool {
 	return false
 }
 
+// delete head from linked list
+func (l *doubleLinkedList) deleteFirstNode() {
+	toDelete := l.head
+	l.head = toDelete.next
+	l.head.prev = nil
+	l.length--
+}
+
+// delete tail from linked list
+func (l *doubleLinkedList) deleteLastNode() {
+	toDelete := l.tail
+	l.tail = toDelete.prev
+	l.tail.next = nil
+	l.length--
+}
+
 func main() {
 	double := doubleLinkedList{}
 	double.addToStart(&node{data: 2})
 	double.addToStart(&node{data: 1})
 
 	double.addToEnd(&node{data: 3})
-	fmt.Println(double.findLastElement())
 	fmt.Println(double.findVal(2))
 	fmt.Println(double.findVal(5))
+	double.addToStart(&node{data: 20})
+	double.addToStart(&node{data: 24})
+	double.addToStart(&node{data: 100})
+
+	double.addToEnd(&node{data: 10})
+	double.addToEnd(&node{data: 132})
+
+	double.printHead()
+	double.printTail()
+
+	double.deleteFirstNode()
+	double.printHead()
+
+	double.deleteFirstNode()
+	double.printHead()
+
+	double.deleteLastNode()
+	double.printTail()
+
+	double.deleteLastNode()
+	double.printTail()
+
+	double.deleteLastNode()
+	double.printTail()
 }
